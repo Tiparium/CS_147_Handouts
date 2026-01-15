d758fa9889939953eef1d44da0997235490461d2
Nainoa Jackson <tiparium@Nainoas-MacBook-Air.local>
2026-01-15 14:23:50 -0800

    Add HW3 register/regFile stubs

---
+++ assignments/hw03/hw3_2/register.v
@@ -0,0 +1,23 @@
+/*
+   CS 147, Spring 26
+   Homework #3, Problem #2
+
+   Parameterized N-bit register built from 1-bit DFFs.
+*/
+module register (
+                 // Outputs
+                 q, err,
+                 // Inputs
+                 clk, rst, en, d
+                 );
+
+   parameter WIDTH = 16;
+
+   input              clk, rst, en;
+   input  [WIDTH-1:0] d;
+   output [WIDTH-1:0] q;
+   output             err;
+
+   /* YOUR CODE HERE */
+
+endmodule
